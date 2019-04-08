9e30dafc48349293949b5960296ff51091855ed5
Matthew Hopkins <mattrhopkins@gmail.com>
2019-04-08 14:57:07 -0600

    functional code

---
+++ app/data/friends.js
@@ -0,0 +1,390 @@
+var friends = [
+    {
+ "name": "Ahmed",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAq7AAAAJDAwYzI4NTQ4LWYwZWUtNGFkYS1hNTYwLTZjYzkwY2ViZDA3OA.jpg",
+        "scores": [
+            "5",
+            "1",
+            "4",
+            "4",
+            "5",
+            "1",
+            "2",
+            "5",
+            "4",
+            "1"
+        ]
+    },
+    {
+        "name": "Jacob Deming",
+        "photo": "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
+        "scores": [
+            "4",
+            "2",
+            "5",
+            "1",
+            "3",
+            "2",
+            "2",
+            "1",
+            "3",
+            "2"
+        ]
+    },
+    {
+        "name": "Jeremiah Scanlon",
+        "photo": "https://avatars2.githubusercontent.com/u/8504998?v=3&s=460",
+        "scores": [
+            "5",
+            "2",
+            "2",
+            "2",
+            "4",
+            "1",
+            "3",
+            "2",
+            "5",
+            "5"
+        ]
+    },
+    {
+        "name": "Louis T. Delia",
+        "photo": "https://pbs.twimg.com/profile_images/639214960049000449/lNCRC-ub.jpg",
+        "scores": [
+            "3",
+            "3",
+            "4",
+            "2",
+            "2",
+            "1",
+            "3",
+            "2",
+            "2",
+            "3"
+        ]
+    },
+    {
+        "name": "Lou Ritter",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAkDAAAAJDhhZTI5NTk2LWQzZjUtNDJjZi1hMTM2LTQ3ZjNmYjE0YmY2NA.jpg",
+        "scores": [
+            "4",
+            "3",
+            "4",
+            "1",
+            "5",
+            "2",
+            "5",
+            "3",
+            "1",
+            "4"
+        ]
+    },
+    {
+        "name": "Jordan Biason",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAisAAAAJGUxYzc4YzA0LWQxMzUtNGI4NS04YTFiLTkwYzM0YTZkNzA2NA.jpg",
+        "scores": [
+            "4",
+            "4",
+            "2",
+            "3",
+            "2",
+            "2",
+            "3",
+            "2",
+            "4",
+            "5"
+        ]
+    },
+    {
+        "name": "Uncle Joe",
+        "photo": "https://placeimg.com/640/480/any",
+        "scores": [
+            "1",
+            "1",
+            "1",
+            "2",
+            "3",
+            "2",
+            "2",
+            "2",
+            "5",
+            "1"
+        ]
+    },
+    {
+        "name": "Amy Gemme",
+        "photo": "www.google.com",
+        "scores": [
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "3",
+            "3",
+            "3"
+        ]
+    },
+    {
+        "name": "den",
+        "photo": "denasd",
+        "scores": [
+            "2",
+            "2",
+            "3",
+            "2",
+            "3",
+            "3",
+            "3",
+            "3",
+            "2",
+            "2"
+        ]
+    },
+    {
+        "name": "Claire Gibeau",
+        "photo": "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwj8w8Xpk7XhAhUPrZ4KHbB9DRAQjRx6BAgBEAU&url=https%3A%2F%2Fwww.discogs.com%2Fartist%2F656013-Mr-Hankey-Poo&psig=AOvVaw1LG5EWUfAFt0o8Wu3xsxPs&ust=1554423197529486.jpg",
+        "scores": [
+            "2",
+            "3",
+            "1",
+            "3",
+            "4",
+            "3",
+            "4",
+            "2",
+            "3",
+            "3"
+        ]
+    },
+    {
+        "name": "Ruili Gao",
+        "photo": "https://images.homedepot-static.com/productImages/e350ef76-f7ff-46ee-83d2-606aab23453c/svn/mea-nursery-rose-bushes-62014-64_1000.jpg",
+        "scores": [
+            "1",
+            "1",
+            "3",
+            "3",
+            "4",
+            "5",
+            "5",
+            "5",
+            "3",
+            "4"
+        ]
+    },
+    {
+        "name": "den",
+        "photo": "aisdhoiafd",
+        "scores": [
+            "2",
+            "3",
+            "2",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "4",
+            "3"
+        ]
+    },
+    {
+        "name": "Rose McClain",
+        "photo": "https://static.stereogum.com/uploads/2018/12/1975-1543857052-640x362.png",
+        "scores": [
+            "3",
+            "3",
+            "3",
+            "1",
+            "5",
+            "3",
+            "3",
+            "4",
+            "4",
+            "1"
+        ]
+    },
+    {
+        "name": "Kamal",
+        "photo": "https://www.google.com/search?biw=1536&bih=722&tbm=isch&sa=1&ei=7mGnXOmXKIHp_Qbzk4O4CA&q=sky&oq=sky&gs_l=img.12..0i67l9j0.10091.10548..12662...0.0..0.79.210.3......1....1..gws-wiz-img.......35i39.uCN1XBFaspw#imgrc=EP_cfwyHq4dqgM:",
+        "scores": [
+            "3",
+            "5",
+            "5",
+            "2",
+            "3",
+            "2",
+            "2",
+            "3",
+            "3",
+            "3"
+        ]
+    },
+    {
+        "name": "PurplePants",
+        "photo": "https://www.funnyhoop.com/pictures/funny-dog-meme/",
+        "scores": [
+            "5",
+            "3",
+            "3",
+            "1",
+            "5",
+            "3",
+            "3",
+            "3",
+            "3",
+            "1"
+        ]
+    },
+    {
+        "name": "Daniel",
+        "photo": "fdsfsd",
+        "scores": [
+            "3",
+            "5",
+            "4",
+            "2",
+            "4",
+            "1",
+            "3",
+            "3",
+            "2",
+            "4"
+        ]
+    },
+    {
+        "name": "Daniel",
+        "photo": "fdsfsd",
+        "scores": [
+            "3",
+            "5",
+            "4",
+            "2",
+            "4",
+            "1",
+            "3",
+            "3",
+            "2",
+            "4"
+        ]
+    },
+    {
+        "name": "Daniel",
+        "photo": "fdsfsd",
+        "scores": [
+            "3",
+            "5",
+            "4",
+            "2",
+            "4",
+            "1",
+            "3",
+            "3",
+            "2",
+            "4"
+        ]
+    },
+    {
+        "name": "ditio",
+        "photo": "https:www.kdf.com",
+        "scores": [
+            "2",
+            "3",
+            "2",
+            "4",
+            "3",
+            "3",
+            "4",
+            "4",
+            "2",
+            "3"
+        ]
+    },
+    {
+        "name": "ditio",
+        "photo": "https:www.kdf.com",
+        "scores": [
+            "3",
+            "3",
+            "3",
+            "2",
+            "3",
+            "2",
+            "3",
+            "3",
+            "3",
+            "2"
+        ]
+    },
+    {
+        "name": "ditio",
+        "photo": "https:www.kdf.com",
+        "scores": [
+            "3",
+            "3",
+            "3",
+            "2",
+            "3",
+            "2",
+            "3",
+            "3",
+            "3",
+            "2"
+        ]
+    },
+    {
+        "name": "Samm",
+        "photo": "https://photos.smugmug.com/Pasadena/Community/HathawaySycamores-Puts-on-a-BBQ/i-nfRwZhh/0/10e5e915/L/8%20Sammy%20Tang%20and%20Valerie%20Macphee%20-1-L.jpg",
+        "scores": [
+            "3",
+            "4",
+            "3",
+            "4",
+            "4",
+            "3",
+            "4",
+            "4",
+            "5",
+            "2"
+        ]
+    },
+    {
+        "name": "Gnome Chompski",
+        "photo": "https://i.kym-cdn.com/entries/icons/mobile/000/001/313/DERP.jpg",
+        "scores": [
+            "5",
+            "5",
+            "5",
+            "1",
+            "5",
+            "1",
+            "1",
+            "1",
+            "5",
+            "1"
+        ]
+    },
+    {
+        "name": "badri",
+        "photo": "image.jpg",
+        "scores": [
+            "1",
+            "3",
+            "1",
+            "3",
+            "3",
+            "1",
+            "5",
+            "3",
+            "4",
+            "3"
+        ]
+    }
+
+]
+
+// Here we export the array. This makes it accessible to other files using require. 
+module.exports = friends;
