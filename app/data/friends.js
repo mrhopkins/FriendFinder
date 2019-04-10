3a0eac8b2b0ec1ab2bf971c4bbc0c3645679f1e2
Matthew Hopkins <mattrhopkins@gmail.com>
2019-04-10 12:01:44 -0600

    updated friends.js data

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 var friends = [
     {
- "name": "Ahmed",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAq7AAAAJDAwYzI4NTQ4LWYwZWUtNGFkYS1hNTYwLTZjYzkwY2ViZDA3OA.jpg",
+ "name": "Stacy Silas",
+        "photo": "https://scontent-den4-1.xx.fbcdn.net/v/t1.0-1/p320x320/49898055_10216604730648159_4790969442399944704_n.jpg?_nc_cat=109&_nc_ht=scontent-den4-1.xx&oh=b8e0df763b5bc025bb917e45120193b9&oe=5D326B42",
         "scores": [
             "5",
             "1",
@@ -16,8 +16,8 @@ var friends = [
         ]
     },
     {
-        "name": "Jacob Deming",
-        "photo": "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
+        "name": "Erin Miller",
+        "photo": "https://scontent-den4-1.xx.fbcdn.net/v/t1.0-1/p320x320/42173485_10101680795411264_5916569526165569536_n.jpg?_nc_cat=108&_nc_ht=scontent-den4-1.xx&oh=0900cea08c218d3c83cd1baf11e4785e&oe=5D3130CF",
         "scores": [
             "4",
             "2",
@@ -32,8 +32,8 @@ var friends = [
         ]
     },
     {
-        "name": "Jeremiah Scanlon",
-        "photo": "https://avatars2.githubusercontent.com/u/8504998?v=3&s=460",
+        "name": "Che Previti DeMarco",
+        "photo": "https://avatars2.githubusercontent.com/u/8504998?v=3&s=460https://scontent-den4-1.xx.fbcdn.net/v/t1.0-1/p320x320/50955222_1992923034160590_387241673110847488_n.jpg?_nc_cat=109&_nc_ht=scontent-den4-1.xx&oh=92179d9e817269edaa0f50d7a300b507&oe=5D333E73",
         "scores": [
             "5",
             "2",
@@ -48,8 +48,8 @@ var friends = [
         ]
     },
     {
-        "name": "Louis T. Delia",
-        "photo": "https://pbs.twimg.com/profile_images/639214960049000449/lNCRC-ub.jpg",
+        "name": "Carly Willis",
+        "photo": "https://scontent-den4-1.xx.fbcdn.net/v/t1.0-1/p320x320/48206613_2497291113620965_6847415829722562560_n.jpg?_nc_cat=100&_nc_ht=scontent-den4-1.xx&oh=34b87c8bf53701256910e1c8c27688bc&oe=5D3EDC40",
         "scores": [
             "3",
             "3",
@@ -64,8 +64,8 @@ var friends = [
         ]
     },
     {
-        "name": "Lou Ritter",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAkDAAAAJDhhZTI5NTk2LWQzZjUtNDJjZi1hMTM2LTQ3ZjNmYjE0YmY2NA.jpg",
+        "name": "Chris Groves",
+        "photo": "https://scontent-den4-1.xx.fbcdn.net/v/t1.0-1/c72.0.320.320a/p320x320/49947887_10217028359477143_8116221444670619648_n.jpg?_nc_cat=109&_nc_ht=scontent-den4-1.xx&oh=b963fb66561f9246784da0101eaa9d5b&oe=5D38C00C",
         "scores": [
             "4",
             "3",
@@ -80,8 +80,8 @@ var friends = [
         ]
     },
     {
-        "name": "Jordan Biason",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAisAAAAJGUxYzc4YzA0LWQxMzUtNGI4NS04YTFiLTkwYzM0YTZkNzA2NA.jpg",
+        "name": "Michael Heinemann",
+        "photo": "https://scontent-den4-1.xx.fbcdn.net/v/t1.0-1/p320x320/23844939_10155845237714857_1815845725643545314_n.jpg?_nc_cat=110&_nc_ht=scontent-den4-1.xx&oh=44b233213bf776d49086fa23ab6b6d8b&oe=5D31C18D",
         "scores": [
             "4",
             "4",
@@ -96,8 +96,8 @@ var friends = [
         ]
     },
     {
-        "name": "Uncle Joe",
-        "photo": "https://placeimg.com/640/480/any",
+        "name": "Joel Morgan",
+        "photo": "https://scontent-den4-1.xx.fbcdn.net/v/t1.0-1/p320x320/49946972_10161206148530705_5847387734854860800_n.jpg?_nc_cat=100&_nc_ht=scontent-den4-1.xx&oh=1f75191ee1c2d12dbbd0e6b021add48d&oe=5D2E26E8",
         "scores": [
             "1",
             "1",
@@ -112,8 +112,8 @@ var friends = [
         ]
     },
     {
-        "name": "Amy Gemme",
-        "photo": "www.google.com",
+        "name": "Ryan Proctor",
+        "photo": "https://scontent-den4-1.xx.fbcdn.net/v/t1.0-1/47577377_1994943533894057_2014385355981062144_n.jpg?_nc_cat=102&_nc_ht=scontent-den4-1.xx&oh=93ccb151f8b0b1dffe51b92222ce03a8&oe=5D385960",
         "scores": [
             "1",
             "1",
@@ -128,8 +128,8 @@ var friends = [
         ]
     },
     {
-        "name": "den",
-        "photo": "denasd",
+        "name": "Scott Smith",
+        "photo": "https://scontent-den4-1.xx.fbcdn.net/v/t1.0-1/c59.59.734.734a/s320x320/35651_4133854790739_1670056247_n.jpg?_nc_cat=107&_nc_ht=scontent-den4-1.xx&oh=01fc6d96b82ba1ca722f5ac7502de095&oe=5D057BEF",
         "scores": [
             "2",
             "2",
@@ -144,232 +144,8 @@ var friends = [
         ]
     },
     {
-        "name": "Claire Gibeau",
-        "photo": "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwj8w8Xpk7XhAhUPrZ4KHbB9DRAQjRx6BAgBEAU&url=https%3A%2F%2Fwww.discogs.com%2Fartist%2F656013-Mr-Hankey-Poo&psig=AOvVaw1LG5EWUfAFt0o8Wu3xsxPs&ust=1554423197529486.jpg",
-        "scores": [
-            "2",
-            "3",
-            "1",
-            "3",
-            "4",
-            "3",
-            "4",
-            "2",
-            "3",
-            "3"
-        ]
-    },
-    {
-        "name": "Ruili Gao",
-        "photo": "https://images.homedepot-static.com/productImages/e350ef76-f7ff-46ee-83d2-606aab23453c/svn/mea-nursery-rose-bushes-62014-64_1000.jpg",
-        "scores": [
-            "1",
-            "1",
-            "3",
-            "3",
-            "4",
-            "5",
-            "5",
-            "5",
-            "3",
-            "4"
-        ]
-    },
-    {
-        "name": "den",
-        "photo": "aisdhoiafd",
-        "scores": [
-            "2",
-            "3",
-            "2",
-            "3",
-            "3",
-            "3",
-            "3",
-            "3",
-            "4",
-            "3"
-        ]
-    },
-    {
-        "name": "Rose McClain",
-        "photo": "https://static.stereogum.com/uploads/2018/12/1975-1543857052-640x362.png",
-        "scores": [
-            "3",
-            "3",
-            "3",
-            "1",
-            "5",
-            "3",
-            "3",
-            "4",
-            "4",
-            "1"
-        ]
-    },
-    {
-        "name": "Kamal",
-        "photo": "https://www.google.com/search?biw=1536&bih=722&tbm=isch&sa=1&ei=7mGnXOmXKIHp_Qbzk4O4CA&q=sky&oq=sky&gs_l=img.12..0i67l9j0.10091.10548..12662...0.0..0.79.210.3......1....1..gws-wiz-img.......35i39.uCN1XBFaspw#imgrc=EP_cfwyHq4dqgM:",
-        "scores": [
-            "3",
-            "5",
-            "5",
-            "2",
-            "3",
-            "2",
-            "2",
-            "3",
-            "3",
-            "3"
-        ]
-    },
-    {
-        "name": "PurplePants",
-        "photo": "https://www.funnyhoop.com/pictures/funny-dog-meme/",
-        "scores": [
-            "5",
-            "3",
-            "3",
-            "1",
-            "5",
-            "3",
-            "3",
-            "3",
-            "3",
-            "1"
-        ]
-    },
-    {
-        "name": "Daniel",
-        "photo": "fdsfsd",
-        "scores": [
-            "3",
-            "5",
-            "4",
-            "2",
-            "4",
-            "1",
-            "3",
-            "3",
-            "2",
-            "4"
-        ]
-    },
-    {
-        "name": "Daniel",
-        "photo": "fdsfsd",
-        "scores": [
-            "3",
-            "5",
-            "4",
-            "2",
-            "4",
-            "1",
-            "3",
-            "3",
-            "2",
-            "4"
-        ]
-    },
-    {
-        "name": "Daniel",
-        "photo": "fdsfsd",
-        "scores": [
-            "3",
-            "5",
-            "4",
-            "2",
-            "4",
-            "1",
-            "3",
-            "3",
-            "2",
-            "4"
-        ]
-    },
-    {
-        "name": "ditio",
-        "photo": "https:www.kdf.com",
-        "scores": [
-            "2",
-            "3",
-            "2",
-            "4",
-            "3",
-            "3",
-            "4",
-            "4",
-            "2",
-            "3"
-        ]
-    },
-    {
-        "name": "ditio",
-        "photo": "https:www.kdf.com",
-        "scores": [
-            "3",
-            "3",
-            "3",
-            "2",
-            "3",
-            "2",
-            "3",
-            "3",
-            "3",
-            "2"
-        ]
-    },
-    {
-        "name": "ditio",
-        "photo": "https:www.kdf.com",
-        "scores": [
-            "3",
-            "3",
-            "3",
-            "2",
-            "3",
-            "2",
-            "3",
-            "3",
-            "3",
-            "2"
-        ]
-    },
-    {
-        "name": "Samm",
-        "photo": "https://photos.smugmug.com/Pasadena/Community/HathawaySycamores-Puts-on-a-BBQ/i-nfRwZhh/0/10e5e915/L/8%20Sammy%20Tang%20and%20Valerie%20Macphee%20-1-L.jpg",
-        "scores": [
-            "3",
-            "4",
-            "3",
-            "4",
-            "4",
-            "3",
-            "4",
-            "4",
-            "5",
-            "2"
-        ]
-    },
-    {
-        "name": "Gnome Chompski",
-        "photo": "https://i.kym-cdn.com/entries/icons/mobile/000/001/313/DERP.jpg",
-        "scores": [
-            "5",
-            "5",
-            "5",
-            "1",
-            "5",
-            "1",
-            "1",
-            "1",
-            "5",
-            "1"
-        ]
-    },
-    {
-        "name": "badri",
-        "photo": "image.jpg",
+        "name": "Eryn McGarry",
+        "photo": "https://scontent-den4-1.xx.fbcdn.net/v/t1.0-1/c0.0.512.512a/s320x320/48429271_10218854252172440_2167361632819216384_n.jpg?_nc_cat=107&_nc_ht=scontent-den4-1.xx&oh=bc8869fab47d610b8b891d90b2f4b4ad&oe=5D34D7F3",
         "scores": [
             "1",
             "3",
